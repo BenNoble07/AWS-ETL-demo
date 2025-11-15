fbfc21130353ff6e38c43037f1f968e1fa8bd023
benedict Noble <bennoble07@gmail.com>
2025-11-15 14:34:13 -0500

    ETL
    Update ETL script to fix CSV name amd paths
    
    Co-Authored-By: Benedict Steele <ben@hyperdimensional.net>

--- scripts/etl.py
+++ scripts/etl.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 # Load sample CSV
-df = pd.read_csv('../data/sample.csv')
+df = pd.read_csv('../data/netflix_sample.csv')
 
 # Simple transformation: rename columns
-df.rename(columns={'old_col':'new_col'}, inplace=True)
+df = df[['show_id', 'type', 'title', 'release_year', 'rating']]
 
 # Save transformed CSV
-df.to_csv('../data/sample_transformed.csv', index=False)
+df.to_csv('../data/netflix_sample_transformed.csv'', index=False)
 
 print("ETL done!")
